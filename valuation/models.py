4d0f244db77815e251f7787879bc9deac430bffe
darksammer <darksammer@gmail.com>
2017-01-11 20:46:02 +0700

    update model

--- valuation/models.py
+++ valuation/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class General_Information(models.Model):
-    short_name = models.CharField(primary_key=True , max_length = 10)
-    full_name = models.CharField(max_length = 100)
+    short_name = models.CharField(primary_key=True, max_length=10)
+    full_name = models.CharField(max_length=100)
 
     #fixed dropdown for fund_type
     LEASE_FREE = 'LF'
@@ -14,33 +14,34 @@ class General_Information(models.Model):
         (LEASE, 'Leasehold'),
         (FREE, 'Freehold'),
     )
-    fund_type = models.CharField(max_length = 20,choices = Fund_Type_Choices,default=LEASE)
+    fund_type = models.CharField(max_length=20, choices=Fund_Type_Choices, default=LEASE)
 
-    property_type = models.CharField(max_length = 50)
+    property_type = models.CharField(max_length=50)
     ipo_date = models.IntegerField()
     dividend_payout_amount_per_year = models.SmallIntegerField()
 
 class Dividend_Yield(models.Model):
     short_name = models.ForeignKey(General_Information, on_delete=models.CASCADE)
-    quarter = models.CharField(max_length = 10)
-    div_yield = models.DecimalField(max_digits=5 , decimal_places=2)
+    quarter = models.CharField(max_length=10)
+    div_yield = models.DecimalField(max_digits=5, decimal_places=2)
 
 class Dividend_Payout(models.Model):
     short_name = models.ForeignKey(General_Information, on_delete=models.CASCADE)
-    quarter = models.CharField(max_length = 10)
-    div_per_share = models.DecimalField(max_digits=8 , decimal_places=5)
-
+    quarter = models.CharField(max_length=10)
+    div_per_share = models.DecimalField(max_digits=8, decimal_places=5)
+    
 class Financial_Statement(models.Model):
     short_name = models.ForeignKey(General_Information, on_delete=models.CASCADE)
-    quarter = models.CharField(max_length = 10)
-    net_asset = models.DecimalField(max_digits=10 , decimal_places=2)
-    net_profit = models.DecimalField(max_digits=10 , decimal_places=2)
-    rental_income = models.DecimalField(max_digits=10 , decimal_places=2)
-    retained_earning = models.DecimalField(max_digits=10 , decimal_places=2)
+    quarter = models.CharField(max_length=10)
+    net_asset = models.DecimalField(max_digits=10, decimal_places=2)
+    net_profit = models.DecimalField(max_digits=10, decimal_places=2)
+    rental_income = models.DecimalField(max_digits=10, decimal_places=2)
+    retained_earning = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Financial_Ratio(models.Model):
     short_name = models.ForeignKey(General_Information, on_delete=models.CASCADE)
-    roe = models.DecimalField(max_digits=5 , decimal_places=2)
-    beta = models.DecimalField(max_digits=5 , decimal_places=2)
-    stability1 = models.DecimalField(max_digits=5 , decimal_places=2)
-    stability2 = models.DecimalField(max_digits=5 , decimal_places=2)
+    quarter = models.CharField(max_length=10)
+    roe = models.DecimalField(max_digits=5, decimal_places=2)
+    beta = models.DecimalField(max_digits=5, decimal_places=2)
+    stability1 = models.DecimalField(max_digits=5, decimal_places=2)
+    stability2 = models.DecimalField(max_digits=5, decimal_places=2)
