59a362f9ffb200dbd75254a2db1cbc423d9af833
darksammer <darksammer@gmail.com>
2017-01-30 21:24:05 +0700

    implement graph with bootstrap

--- valuation/views.py
+++ valuation/views.py
@@ -12,7 +12,7 @@ def chart_view(request, name):
                     'year','price','fair','short_name']}
             ])
 
-    cht = Chart(
+    cht1 = Chart(
         datasource = data,
         series_options = 
         [{'options':{
@@ -39,4 +39,32 @@ def chart_view(request, name):
             'title': {
                 'text': 'time'}}})
 
-    return render_to_response('valuation/chart.html', {'chart': cht})
+
+    cht2 = Chart(
+        datasource = data,
+        series_options = 
+        [{'options':{
+                'type': 'area',
+                'stacking': False,
+                'fillOpacity': 0.1,
+                'color': '#000000',
+                },
+            'terms':{
+                'year':['price'],
+                }},
+         {'options':{
+                'type': 'line',
+                'stacking' : False,
+                'dashStyle' : 'longdash',
+                'color': '#000000'},
+            'terms':{
+                'year':['fair']
+         }}],
+        chart_options =
+         {'title': {
+             'text': 'sample chart'},
+        'xAxis': {
+            'title': {
+                'text': 'time'}}})
+
+    return render(request,'valuation/chart.html', {'chart_list': [cht1,cht2],})
