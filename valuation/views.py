e4d24a919e97c4ab7d3c42ac3e4052f4661effdf
darksammer <darksammer@gmail.com>
2017-03-15 21:20:02 +0700

    fix bug in ranking , remove fair ranking

--- valuation/views.py
+++ valuation/views.py
@@ -1,24 +1,38 @@
 from django.shortcuts import render,render_to_response, redirect
 from django.http import Http404
-from django.db.models import F,Prefetch
+from django.db.models import F, Prefetch, Max
 from django.utils import timezone
 from chartit import DataPool, Chart
 from .models import *
 
 # Create your views here.
 def index_view(request):
-    fund_list = Dividend_Yield.objects.raw('select valuation_dividend_yield.id, max(valuation_dividend_yield.period) as period, valuation_dividend_yield.div_yield,\
-                                                valuation_dividend_yield.short_name_id , valuation_general_information.full_name from valuation_dividend_yield\
-                                                join valuation_general_information on valuation_dividend_yield.short_name_id = valuation_general_information.short_name\
-                                                group by short_name_id\
-                                                order by div_yield DESC, period DESC\
-                                                limit 5')
+    fund_list = Dividend_Yield.objects.prefetch_related(
+                                Prefetch(
+                                    "period",
+                                    queryset = Period_Table.objects.all()
+                                )
+                            ).order_by('-div_yield','-period')[:5]
+
+    # fund_list = Dividend_Yield.objects.raw('select valuation_dividend_yield.id, max(valuation_dividend_yield.period) as period, valuation_dividend_yield.div_yield,\
+    #                                             valuation_dividend_yield.short_name_id , valuation_general_information.full_name from valuation_dividend_yield\
+    #                                             join valuation_general_information on valuation_dividend_yield.short_name_id = valuation_general_information.short_name\
+    #                                             group by short_name_id\
+    #                                             order by div_yield DESC, period DESC\
+    #                                             limit 5')
     
-    best_fund = Fair_Value.objects.raw('select id, short_name_id, period, price, fair\
-                                                    from valuation_fair_value\
-                                                    where short_name_id = (select short_name_id from valuation_dividend_yield\
-                                                                            order by div_yield DESC limit 1)\
-                                                    group by short_name_id, period')
+    best_fund = Fair_Value.objects.prefetch_related(
+                                Prefetch(
+                                    "period",
+                                    queryset = Period_Table.objects.all()
+                                )
+                            )
+
+    # best_fund = Fair_Value.objects.raw('select id, short_name_id, period, price, fair\
+    #                                                 from valuation_fair_value\
+    #                                                 where short_name_id = (select short_name_id from valuation_dividend_yield\
+    #                                                                         order by div_yield DESC limit 1)\
+    #                                                 group by short_name_id, period')
 
     data = \
         DataPool(series=
@@ -233,20 +247,14 @@ def fund_view(request,name):
 def ranking_view(request,rank_type):
     #ranking by latest_yield
     if rank_type == "yield":
-        fund_list = Dividend_Yield.objects.raw('select valuation_dividend_yield.id, max(valuation_dividend_yield.period) as period, valuation_dividend_yield.div_yield,\
-                                                valuation_dividend_yield.short_name_id , valuation_general_information.full_name from valuation_dividend_yield\
+        fund_list = Dividend_Yield.objects.raw('select valuation_dividend_yield.id, valuation_dividend_yield.period_id, valuation_dividend_yield.div_yield,\
+                                                valuation_dividend_yield.short_name_id , valuation_general_information.full_name\
+                                                from valuation_dividend_yield\
                                                 join valuation_general_information on valuation_dividend_yield.short_name_id = valuation_general_information.short_name\
-                                                group by short_name_id\
-                                                order by div_yield DESC, period DESC')
+                                                where valuation_dividend_yield.id in (\
+                                                                    select max(valuation_dividend_yield.id)\
+                                                                    from valuation_dividend_yield\
+                                                                    group by valuation_dividend_yield.short_name_id\
+                                                                )\
+                                                order by valuation_dividend_yield.period_id DESC')
         return render(request, 'valuation/ranking.html' , {'fund_list':fund_list, 'rank_type':rank_type})
-    #ranking by price&fair
-    elif rank_type == "fair":
-        fund_list = Fair_Value.objects.raw('select valuation_fair_value.id , valuation_fair_value.short_name_id , valuation_general_information.full_name , valuation_fair_value.price - valuation_fair_value.fair as diff ,\
-                                            max(valuation_fair_value.period) as period from valuation_fair_value\
-                                            join valuation_general_information on valuation_fair_value.short_name_id = valuation_general_information.short_name\
-                                            group by short_name_id\
-                                            order by diff DESC, period')
-        return render(request, 'valuation/ranking.html' , {'fund_list':fund_list, 'rank_type':rank_type})
-    #anything else
-    else:
-        raise Http404("Page not found")
