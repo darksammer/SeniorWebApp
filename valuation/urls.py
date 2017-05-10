1935f717855671e222f66a897c4f678dc065f941
darksammer <darksammer@gmail.com>
2017-05-10 16:36:48 +0700

    change index url form /index to root url

--- valuation/urls.py
+++ valuation/urls.py
@@ -3,10 +3,5 @@ from . import views
 
 app_name = 'valuation'
 urlpattern = [
-    url(r'^$',views.fund_view,name='fund'),
-    url(r'^$',views.index_view,name='index'),
-    url(r'^$',views.search_view,name='search'),
-    url(r'^$',views.ranking_view,name='ranking'),
-    url(r'^$',views.test_page,name='test'),
-    url(r'^$',views.news_view,name='news')
+    
 ]
